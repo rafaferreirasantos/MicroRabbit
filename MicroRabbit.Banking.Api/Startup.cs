da63b7cb4dea2c6525a2648122503afaafcdd684
Rafael Ferreira dos Santos <osmarditto@gmail.com>
2020-08-10 20:24:10 -0300

    Migration bug fixed

--- MicroRabbit.Banking.Api/Startup.cs
+++ MicroRabbit.Banking.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using MicroRabbit.Baking.Data.Context;
 using MicroRabbit.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
@@ -26,9 +27,12 @@ namespace MicroRabbit.Banking.Api
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
       services.AddControllers();
+      services.AddMediatR(typeof(Startup));
+
       services.AddDbContext<BankingDbContext>(options => {
         options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"));
       });
+
       RegisterServices(services);
     }
 
