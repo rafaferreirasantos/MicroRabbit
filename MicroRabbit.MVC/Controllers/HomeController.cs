2436128de8a030e6779910440919b22d3b192736
Rafael Ferreira dos Santos <osmarditto@gmail.com>
2020-08-11 10:30:49 -0300

    Final project

---
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Services;
+using MicroRabbit.MVC.Models.DTO;
+
+namespace MicroRabbit.MVC.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly ITransferService _trnasferService;
+
+
+    public HomeController(ILogger<HomeController> logger, ITransferService transferService)
+    {
+      _logger = logger;
+      _trnasferService = transferService;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    [HttpPost]
+    public async Task<IActionResult> Transfer(TransferViewModel model)
+    {
+      TransferDTO transferDTO = new TransferDTO()
+      {
+        FromAccount = model.FromAccount,
+        ToAccount = model.ToAccount,
+        TransferAmount = model.TransferAmmount,
+      };
+      await _trnasferService.Transfer(transferDTO);
+      return View("Index");
+    }
+  }
+}
